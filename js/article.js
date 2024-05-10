924b6f5c0f94755d85fd3e70de607a2badd8e2eb
solaez <aguerrerosolaez@gmail.com>
2024-05-10 14:28:55 -0300

    1

--- js/article.js
+++ js/article.js
@@ -38,4 +38,44 @@ let slideIndex = 0;
       autoSlide(); // Reinicia el carrusel automático
     }
     //fin------------------------------------------------------------
+    //ventas inicio_____---------------------------------------------
+    let slideIndex2 = 0;
+    let timer2;
+function showSlides2() {
+  const slides2 = document.querySelector('.carousel2');
+  slides2.style.transform = `translateX(-${slideIndex2 * 50}%)`;
+}
+
+function nextSlide2() {
+  if (slideIndex2 < 3) {
+    slideIndex2++;
+  } else {
+    slideIndex2 = 0;
+  }
+  showSlides2();
+}
+
+function prevSlide2() {
+  if (slideIndex2 > 0) {
+    slideIndex2--;
+  } else {
+    slideIndex2 = 3;
+  }
+  showSlides2();
+}
+
+function autoSlide2() {
+  timer2 = setInterval2(() => {
+    nextSlide2();
+  }, 3000); // Cambia 3000 a la cantidad de milisegundos que desees
+}
+
+autoSlide2(); // Comienza el carrusel automático
+
+function resetTimer2() {
+  clearInterval2(timer2); // Detiene el intervalo actual
+  autoSlide2(); // Reinicia el carrusel automático
+}
+//fin------------------------------------------------------------
+
     
