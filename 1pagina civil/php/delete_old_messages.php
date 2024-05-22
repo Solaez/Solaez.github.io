4dd96547f0c9d8a0ce4e2154d5ab6f8d337ea482
solaez <aguerrerosolaez@gmail.com>
2024-05-22 08:29:34 -0300

    gfg

---
+++ 1pagina civil/php/delete_old_messages.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "www.db4free.net";
+$username = "root";
+$password = "";
+$dbname = "mario_kart_8";
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+// Eliminar mensajes con más de 2 minutos de antigüedad
+$sql = "DELETE FROM messages WHERE timestamp < (NOW() - INTERVAL 2 MINUTE)";
+if ($conn->query($sql) === TRUE) {
+    echo "Mensajes antiguos eliminados correctamente.";
+} else {
+    echo "Error al eliminar mensajes: " . $conn->error;
+}
+
+$conn->close();
+?>
