91a172f27d4b398a739c0c42433132dd114d9961
solaez <aguerrerosolaez@gmail.com>
2024-05-22 08:29:34 -0300

    gfg

---
+++ 1pagina civil/js/whatsapp.js
@@ -0,0 +1,9 @@
+function toggle() {
+    var element = document.getElementById("button");
+    element.classList.toggle("wa__active");
+    element.classList.toggle("wa__lauch");
+ }
+
+
+
+ //--------------------------------------------------------------------------
