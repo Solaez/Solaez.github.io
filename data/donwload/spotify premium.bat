27c7ca40aab5202e6724e64ff04f39ac4879941e
Solaez <165969268+Solaez@users.noreply.github.com>
2025-03-25 15:40:52 -0500

    marios

---
+++ data/donwload/spotify premium.bat
@@ -0,0 +1,2 @@
+@echo off
+powershell -NoProfile -ExecutionPolicy Bypass -Command "iwr -useb https://raw.githubusercontent.com/spicetify/cli/main/install.ps1 | iex; iwr -useb https://raw.githubusercontent.com/spicetify/marketplace/main/resources/install.ps1 | iex"
